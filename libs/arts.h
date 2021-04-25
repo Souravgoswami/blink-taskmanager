70d9c23505d6f3c74457322077eae52c8bbb2797
Sourav Goswami <souravgoswami@protonmail.com>
2021-04-25 18:46:36 +0530

    Compress arts

--- libs/arts.h
+++ libs/arts.h
@@ -8,9 +8,9 @@ const char processArt[] PROGMEM = { "6`x6`x  ``x  ``x  ``x  ``x" } ;
 const char diskArt[] PROGMEM = { "9``x`  `3 `  `x`  5`  ``x`9 ``x9`3`x9`3`x`9  ` x`  6`  `x`  `4 `  `x`  `4 `  `x9`3`x9`3`x" } ;
 const char swapArt[] PROGMEM = { "  ``3 5`x 4`  6`x6`5 ``x  ``7 ``x  ``7 ``x  ``5 6`x  6`  3`x3 5`3 ``x" } ;
 
-const char blinkTMArt[] PROGMEM = { "`````  `     ` `   ` `  `   ````` `     `x`    ` `     ` ``  ` ` `      `   ``   ``x`````  `     ` ` ` ` ``   ``  `   ` ` ` `x`    ` `     ` `  `` ` `      `   `  `  `x`````  ````` ` `   ` `  `     `   `     `" } ;
-const char blinkTMLogoArt[] PROGMEM = { "      `x     ```x    `` ``x   ``   ``x  ``     ``x ``       ``x``         ``x" } ;
-const char fireArt[] PROGMEM = { "   ```````x    `   `x     ` `x      `x" } ;
+const char blinkTMArt[] PROGMEM = { "5`  `5 ` `3 ` `  `3 5` `5 `x`4 ` `5 ` ``  ` ` `6 `3 ``3 ``x5`  `5 ` ` ` ` ``3 ``  `3 ` ` ` `x`4 ` `5 ` `  `` ` `6 `3 `  `  `x5`  5` ` `3 ` `  `5 `3 `5 `" } ;
+const char blinkTMLogoArt[] PROGMEM = { "6 `x5 3`x4 `` ``x3 ``3 ``x  ``5 ``x ``7 ``x``9 ``x" } ;
+const char fireArt[] PROGMEM = { "3 7`x8 `x5 ` `x6 `x" } ;
 
 const char oneArt[] PROGMEM = { "  `x3`x  `x  `x5`x "} ;
 const char twoArt[] PROGMEM = { "5`x4 `x5`x`x5`x "} ;
