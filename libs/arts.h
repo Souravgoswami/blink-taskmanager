ab324ef51e57e4c78931f6dd72426b8107597219
Sourav Goswami <souravgoswami@protonmail.com>
2021-04-25 18:55:44 +0530

    Fix fire art

--- libs/arts.h
+++ libs/arts.h
@@ -10,8 +10,7 @@ const char swapArt[] PROGMEM = { "  ``3 5`x 4`  6`x6`5 ``x  ``7 ``x  ``7 ``x  ``
 
 const char blinkTMArt[] PROGMEM = { "5`  `5 ` `3 ` `  `3 5` `5 `x`4 ` `5 ` ``  ` ` `6 `3 ``3 ``x5`  `5 ` ` ` ` ``3 ``  `3 ` ` ` `x`4 ` `5 ` `  `` ` `6 `3 `  `  `x5`  5` ` `3 ` `  `5 `3 `5 `" } ;
 const char blinkTMLogoArt[] PROGMEM = { "6 `x5 3`x4 `` ``x3 ``3 ``x  ``5 ``x ``7 ``x``9 ``x" } ;
-const char fireArt[] PROGMEM = { "3 7`x8 `x5 ` `x6 `x" } ;
-
+const char fireArt[] PROGMEM = { "3 7` x4 `3 `x5 ` `x6 `x" } ;
 const char oneArt[] PROGMEM = { "  `x3`x  `x  `x5`x "} ;
 const char twoArt[] PROGMEM = { "5`x4 `x5`x`x5`x "} ;
 const char threeArt[] PROGMEM = { "4` x3 `x4`x3 `x4`x "} ;
