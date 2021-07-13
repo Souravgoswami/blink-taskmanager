1a790fba75ae818fb2485041100e81cf08b5c5fc
Sourav Goswami <souravgoswami@protonmail.com>
2021-07-14 04:14:09 +0530

    New particles effect - with less memory footprint

--- libs/particles.h
+++ libs/particles.h
@@ -1,35 +1,35 @@
 #ifndef PARTICLES
-#define PARTICLES 15
+#define PARTICLES 30
 #endif
 
 // Paint the particles
-unsigned char particles[PARTICLES][3] ;
+unsigned char particles[PARTICLES][2] ;
 
 void initParticles() {
-	unsigned char *p ;
+	static unsigned char *p ;
 
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
 
 		p[0] = rand() % 128 ;
-		p[1] = rand() % 64 * -1 ;
-		p[2] = rand() % 3 + 1 ;
+		p[1] = rand() % 64 ;
 	}
 }
 
 void drawParticles() {
-	unsigned char *p ;
+	static unsigned char *p ;
 
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
-		p[1] += p[2] ;
 
-		if (p[1] > 64) {
+		p[0] += round(sin(i)) ;
+		p[1] += round(cos(i)) ;
+
+		if (p[1] > 64 || p[0] > 127 || p[1] < 1 || p[0] < 1) {
 			p[0] = rand() % 128 ;
-			p[1] = rand() % 64 * -1 ;
-			p[2] = rand() % 3 + 1 ;
-		} else if (rand() % 10 < 8) {
-			display.drawPixel(p[0], p[1], WHITE) ;
+			p[1] = rand() % 64 ;
 		}
+
+		display.drawPixel(p[0], p[1], WHITE) ;
 	}
 }
