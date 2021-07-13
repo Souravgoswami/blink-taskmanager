38fef508492124a117f89296b9ffd0c42c4ed58a
Sourav Goswami <souravgoswami@protonmail.com>
2021-07-14 05:17:45 +0530

    Make particles.h more efficient

--- libs/particles.h
+++ libs/particles.h
@@ -24,12 +24,12 @@ void drawParticles() {
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
 
-		p[0] += p[2] ;
-		p[1] += p[3] ;
-
-		if (p[1] > 64 || p[0] > 127 || p[1] < 1 || p[0] < 1) {
+		if ((p[0] & 127) == 0 || (p[1] & 63) == 0) {
 			p[0] = rand() & 127 ;
 			p[1] = rand() & 63 ;
+		} else {
+			p[0] += p[2] ;
+			p[1] += p[3] ;
 		}
 
 		display.drawPixel(p[0], p[1], WHITE) ;
