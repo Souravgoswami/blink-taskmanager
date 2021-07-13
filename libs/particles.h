572a8695bb7e92ee5846893408c8be7ea4db04e7
Sourav Goswami <souravgoswami@protonmail.com>
2021-07-14 04:59:16 +0530

    Reduce calculation in particles.h

--- libs/particles.h
+++ libs/particles.h
@@ -3,7 +3,7 @@
 #endif
 
 // Paint the particles
-unsigned char particles[PARTICLES][2] ;
+unsigned char particles[PARTICLES][4] ;
 
 void initParticles() {
 	static unsigned char *p ;
@@ -11,8 +11,10 @@ void initParticles() {
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
 
-		p[0] = rand() % 128 ;
-		p[1] = rand() % 64 ;
+		p[0] = rand() & 127 ;
+		p[1] = rand() & 63 ;
+		p[2] = round(sin(i)) ;
+		p[3] = round(cos(i)) ;
 	}
 }
 
@@ -22,12 +24,12 @@ void drawParticles() {
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
 
-		p[0] += round(sin(i)) ;
-		p[1] += round(cos(i)) ;
+		p[0] += p[2] ;
+		p[1] += p[3] ;
 
 		if (p[1] > 64 || p[0] > 127 || p[1] < 1 || p[0] < 1) {
-			p[0] = rand() % 128 ;
-			p[1] = rand() % 64 ;
+			p[0] = rand() & 127 ;
+			p[1] = rand() & 63 ;
 		}
 
 		display.drawPixel(p[0], p[1], WHITE) ;
