d6af245ddeb8af654a80c7d6e93b91bce3905e52
Sourav Goswami <souravgoswami@protonmail.com>
2021-07-30 19:48:45 +0530

    Bugs fixed

--- libs/particles.h
+++ libs/particles.h
@@ -1,21 +1,28 @@
 #ifndef PARTICLES
-#define PARTICLES 30
+#define PARTICLES 55
 #endif
 
 #ifndef PARTICLES_FALL
-#define PARTICLES_FALL 5
+#define PARTICLES_FALL 10
 #endif
 
 // Paint the particles
 unsigned char particles[PARTICLES][2] ;
+unsigned char fallen = 0 ;
 unsigned char *particleFall[PARTICLES_FALL] ;
 unsigned char *p ;
 
 void initParticles() {
 	for(unsigned char i = 0 ; i < PARTICLES ; ++i) {
 		p = particles[i] ;
+
 		p[0] = rand() & 127 ;
 		p[1] = rand() & 63 ;
+	}
+
+	for(unsigned char i = 0 ; i < PARTICLES_FALL ; ++i) {
+		fallen = fallen < PARTICLES ? fallen + 1 : 0 ;
+		particleFall[i] = particles[fallen] ;
 	}
 }
 
@@ -23,12 +30,16 @@ void drawParticles() {
 	for(unsigned char i = 0 ; i < PARTICLES_FALL ; ++i) {
 		p = particleFall[i] ;
 
-		if (p[1] & 63) {
-			p[1] += 1 ;
+		if (p[1] < 63) {
+			p[1] += (p[0] & 1) + 1 ;
 		} else {
 			p[0] = rand() & 127 ;
 			p[1] = rand() & 63 ;
-			particleFall[i] = particles[rand() % PARTICLES] ;
+
+			// fallen = (fallen + 1) % PARTICLES // << Waste of processing
+			fallen = fallen < PARTICLES ? fallen + 1 : 0 ;
+
+			particleFall[i] = particles[fallen] ;
 		}
 	}
 
